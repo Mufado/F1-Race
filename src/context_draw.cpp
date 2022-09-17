e71918300949339a95f29dffaf01dd875047d284
Gustavo Girão <gustavogiraobg@gmail.com>
2022-09-17 20:37:49 -0300

    chore: minnor changes

--- src/context_draw.cpp
+++ src/context_draw.cpp
@@ -10,14 +10,14 @@ void perspective_init(sf::Window *window)
     sf::Vector2u windows_size = window->getSize();
     float aspect_ratio = (float) (windows_size.x / windows_size.y);
 
-    glm::mat4 idt_mat = glm::mat4(1.0);
-    glLoadMatrixf(glm::value_ptr(idt_mat));
+    glm::mat4 identity_mat = glm::mat4(1.0f);
+    glLoadMatrixf(glm::value_ptr(identity_mat));
 
     glMatrixMode(GL_PROJECTION);
 
-    glm::mat4 proj_mat = glm::perspective(60.f, aspect_ratio, 1.f, 1000.f);
+    glm::mat4 projection_mat = glm::perspective(60.0f, aspect_ratio, 1.0f, 1000.0f);
 
-    glLoadMatrixf(glm::value_ptr(proj_mat));
+    glLoadMatrixf(glm::value_ptr(projection_mat));
 }; 
 
 void draw_context()
