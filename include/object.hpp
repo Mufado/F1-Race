3da75649486eaec683502b4243924a272c2131a7
João Gerliandro <joaogerliandro@gmail.com>
2022-09-28 08:41:30 -0300

    Object class update

--- include/object.hpp
+++ include/object.hpp
@@ -15,13 +15,17 @@ typedef struct
 
 typedef struct
 {
-    std::vector<Vertex> vertex_list;
+    std::vector<Vertex>  vertex_list;
+    std::vector<int32_t> face_indices;
 } Meshe;
 
 typedef struct 
 {
+    glm::fvec3 global_position;
+    glm::fvec3 local_position; #This is the object self axis
+    glm::fvec3 color;
+    
     Meshe obj_meshe; 
-
 } Object;
 
 Object load_obj_from_file(const std::string);
