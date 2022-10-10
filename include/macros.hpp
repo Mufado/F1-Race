d67e21f7dd09434013192f00e53ce49626db04a4
João Gerliandro <joaogerliandro@gmail.com>
2022-10-10 16:48:02 -0300

    refactor - minor changes

--- include/macros.hpp
+++ include/macros.hpp
@@ -2,7 +2,7 @@
 #include <libs_init.hpp>
 
 //Object Module Macros
-#define MESHES_FILEPATH "../../../assets/meshes/"
+#define MESHES_FILEPATH   "../../../assets/meshes/"
 #define DEFAULT_LOCAL_POS glm::fvec3(0.0f, 0.0f, 0.0f)
 
 //Game Module Macros
@@ -10,18 +10,18 @@
 #define DEBUG_COLOR   glm::fvec4(0.3f, 0.3f, 0.3f, 1.0f)
 
 //Draw Module Macros
-#define TERRAIN_SIZE          200.0f
-#define HIGHWAY_WIDTH         3.0f
-#define TRACKS_SIZE glm::fvec3(0.15f, 2.0f, 1.0f)
-#define AXIS_SIZE             TERRAIN_SIZE
+#define TERRAIN_SIZE  200.0f
+#define HIGHWAY_WIDTH 3.0f
+#define TRACKS_SIZE   glm::fvec3(0.15f, 2.0f, 1.0f) //Width, Length, Detachment
+#define AXIS_SIZE     TERRAIN_SIZE
 
 //Camera Module Macros
-#define SIGTH_LINE_WIDTH  3.0f
-#define SIGHT_SIZE        5.0f
-#define SIGHT_COLOR       glm::fvec3(0.4f, 0.3f, 0.4f)
+#define SIGTH_LINE_WIDTH 3.0f
+#define SIGHT_SIZE       5.0f
+#define SIGHT_COLOR      glm::fvec3(0.4f, 0.3f, 0.4f)
 
 #define CAM_VIEW    glm::fvec2(2.5f, TERRAIN_SIZE)
 #define CAM_UP_AXIS glm::fvec3(0.0f, 1.0f, 0.0f)
 
 //Car Object Macros
-#define DEFAULT_CAR_POS   glm::fvec3(0.0f, 0.03f, -6.0f)
+#define DEFAULT_CAR_POS glm::fvec3(0.0f, 0.03f, -6.0f)
