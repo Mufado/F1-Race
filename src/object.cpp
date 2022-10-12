7e41bf07765996c594958125e98106b9332ba809
João Gerliandro <joaogerliandro@gmail.com>
2022-10-12 10:38:59 -0300

    refactor - object loader refactor

--- src/object.cpp
+++ src/object.cpp
@@ -2,76 +2,49 @@
 
 Object load_obj_from_file(const std::string obj_file)
 {
-    Object loaded_obj;
-
-    //Vertexs
-    std::vector<glm::fvec3> vertexs_positions;
-    //Faces
-    std::vector<GLint>      vertexs_indicies;
+    Object loaded_object;
     
-    //Vertexs e temp array
-    std::vector<glm::fvec3>  polygon_vertexs;
+    std::ifstream filestream{ MESHES_FILEPATH + obj_file };
 
-    std::stringstream str_stream;
-    std::ifstream     in_file(MESHES_FILEPATH + obj_file);
-
-    std::string       line        = "",
-                      line_prefix = "";
-
-    //Aux atributes 
-    GLint     aux_glint = 0;
-    glm::vec2 aux_vec2; 
-    glm::vec3 aux_vec3;
-
-    if(!in_file.is_open())
+    if (!filestream || !filestream.is_open())
         throw "ERRO: Can't open the file !";
 
-    while(std::getline(in_file, line))
+    std::vector<glm::vec3> vertex_list;
+
+    glm::vec3 vertex{};
+    glm::vec3 indice{};
+
+    for (std::string line; std::getline(filestream, line); )
     {
-        str_stream.clear();
-        str_stream.str(line);
-        str_stream >> line_prefix;
+        if (line.empty())
+            continue;
 
-        if(line_prefix == "v")
+        size_t index{ line.find_first_of(' ') };
+
+        std::string_view line_view = line;
+        std::string_view prefix = line_view.substr(0, index);
+        std::string_view target = line_view.substr(index);
+
+        std::istringstream stream{ target.data() };
+
+        if (prefix == "v")
         {
-            str_stream >> aux_vec3.x >> aux_vec3.y >> aux_vec3.z;
-            vertexs_positions.push_back(aux_vec3);
+            if (stream >> vertex.x >> vertex.y >> vertex.z)
+                vertex_list.push_back(std::move(vertex));
+
+            stream.ignore('\n');
+            if (stream)
+                throw "Vertex Coordinates must be 3D";
         }
-        else if(line_prefix == "f")
+        else if (prefix == "f")
         {
-            int32_t counter = 0;
-            while(str_stream >> aux_glint)
-            {
-                if(counter == 0)
-                    vertexs_indicies.push_back(aux_glint);
+            std::size_t vertex_index{};
+            char delim{};
 
-                if(str_stream.peek() == '/')
-                {
-                    counter++;
-                    str_stream.ignore(1, '/');
-                }
-                else if(str_stream.peek() == ' ')
-                {
-                    counter++;
-                    str_stream.ignore(1, ' ');
-                }
-
-                if(counter > 2)
-                    counter = 0;
-            }
+            while (stream >> vertex_index >> delim)
+                loaded_object.obj_meshe.vertex_list.emplace_back(std::move(vertex_list[vertex_index - 1u]));
         }
-
-        polygon_vertexs.resize(
-            vertexs_indicies.size(),
-            glm::fvec3()                    
-        );
-
-        for(size_t i = 0; i < polygon_vertexs.size(); i++)
-            polygon_vertexs[i] = vertexs_positions[vertexs_indicies[i] - 1];
-
     }
 
-    loaded_obj.obj_meshe.vertex_list = polygon_vertexs;
-
-    return loaded_obj;
+    return loaded_object;
 }
